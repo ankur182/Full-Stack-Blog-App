db99ba3d048fc0715332d1f89698520d887adf3e
ankur <ankurgautam009@gmail.com>
2025-01-06 18:43:33 +0530

    commit message

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Here from '../components/Here'
+import Blogs from '../components/Blogs'
+import Fotter from '../components/Fotter'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Here/>
+      <Blogs/>
+      <Fotter/>
+    </>
+  )
+}
+
+export default Home
